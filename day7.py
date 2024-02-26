3c5dc3adaca04962201de7add262c93921ff623a
SailaxmiKomirineni <134919013+SailaxmiKomirineni@users.noreply.github.com>
2024-02-26 23:31:14 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,23 @@
+Time Complexcity:
+  Efficient time to run an algorithm is time complexcity. It is denoted by "Big O" notaion. Big O follows worst case to get the given value.
+    
+    => List Time Complexcity :
+    1. append() -> O(1) directly adds at end.
+    2. remove() -> O(n) searchs the given value from 0 to n index takes n sec
+    3. pop()    -> O(1) it doesn't searchs, directly removes last element or specified index element 
+
+    => Set Time Complexcity :
+    1. add() -> O(1)
+    2. remove() -> O(1) 
+      In set hash table also called as hash maps or dictionaries. Which makes id for every values to move directly to the value.
+
+Different types of time complexcity are:-
+1. O(1)       : constant Time
+2. O(log n)   : Logarithmic Time
+3. O(n)       : Linear Time
+4. O(n log n) : Linearithmic Time
+5. O(n^2)     : Quadritic Time                                                                             
+6. O(n^k)     : Polynomial Time
+7. O(2^n)     : Exponential Time
+8. O(n!)      : Factorial Time
+                                                                              
